bb9cb77566687f4a04da9c5f1cdf6a1cfe2288e4
AlanMarcial16 <amarcial1699@hotmail.com>
2021-11-09 10:23:29 -0600

    cambio 9 de noviembre

--- index.php
+++ index.php
@@ -22,17 +22,17 @@ $body = $request->getParsedBody();
 $result = $wsventas->__soapCall('getProd', array(
     'user'=>$request["user"], 'pass'=>$request["pass"], 'categoria'=>$request["categoria"]));
     $json = array('code'=>$result->code, 'message'=>$result->message, 'data'=>$result->data, 'status'=>$result->status);
-    $response->getBody()->write(var_dump($body));
+    $response->getBody()->write(json_encode($json, JSON_PRETTY_PRINT));
 return $response;
 });
 
 $app->put('/updateProd[/{isbn}]', function ( Request $request, Response $response, $args ) {
 global $wsalmacen;
 $body = $request->getParsedBody();
-$response->getBody()->write($body);
-/*$result = $wsalmacen->__soapCall('updateProd', array('user'=>$body["user"], 'pass'=>$body["pass"], 'detalles'=>$body["detalles"], 'isbn'=>$args["isbn"]));
+$result = $wsalmacen->__soapCall('updateProd', array(
+    'user'=>$body["user"], 'pass'=>$body["pass"], 'detalles'=>$body["detalles"], 'isbn'=>$args["isbn"]));
     $json = array('code'=>$result->code, 'message'=>$result->message, 'data'=>$result->data, 'status'=>$result->status);
-    $response->getBody()->write(json_encode($json));*/
+    $response->getBody()->write(json_encode($json, JSON_PRETTY_PRINT));
 });
 
 $app->run();
