16b4019aebdb2dc1625ef2b4f05041f23105a639
AlanMarcial16 <amarcial1699@hotmail.com>
2021-11-08 19:13:39 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+use Slim\Exception\NotFoundException;
+
+require 'vendor/autoload.php';
+
+$app = AppFactory::create();
+$app->addRoutingMiddleware();
+$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+
+$app->get('/', function ( Request $request, Response $response, $args ) {
+$response->getBody()->write("Hola Mundo Slim!!");
+return $response;
+});
+
+$app->get('/hola[/{nombre}]', function( Request $request, Response $response, $args ) {
+    $response->getBody()->write("Hola ".$args["nombre"]);
+    return $response;
+});
+
+$app->run();
+
+
+
+?>
